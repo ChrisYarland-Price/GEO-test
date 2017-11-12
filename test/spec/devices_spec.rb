78d66652f989b40d4a22a18ab737d302c451da48
Chris Yarland-Price <chris.yarland@protonmail.com>
2017-11-12 12:18:55 +0000

    Uncommented some tests

--- test/spec/devices_spec.rb
+++ test/spec/devices_spec.rb
@@ -8,8 +8,8 @@ describe "Devices api" do
   end
 
   it "should return all devices notes when a get request is received." do
-    # expect(DevicesApiTest.get_all_devices.code).to eql(200) 
-    # expect(DevicesApiTest.get_all_devices.parsed_response).to be_instance_of Hash
+    expect(DevicesApiTest.get_all_devices.code).to eql(200) 
+    expect(DevicesApiTest.get_all_devices.parsed_response).to be_instance_of Hash
     notes =  Nokogiri::XML(DevicesApiTest.get_all_devices.to_s).search('notes')
     puts notes[0]
     expect(notes[0].text).to eql('Cosy hub, Ethernet microcontroller')
